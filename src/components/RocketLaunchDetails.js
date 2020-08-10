fbe7d4f32cc7f21a58549cc7229c3e174cfeff57
Jasvinder Singh <jvsinghk@gmail.com>
2020-08-10 06:37:28 +0530

    rocket details card styles

--- src/components/RocketLaunchDetails.js
+++ src/components/RocketLaunchDetails.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import './RocketLaunchDetails.css'
 
 function RocketLaunchDetails({ details }) {
     const { flight_number, mission_name, mission_id, launch_year, launch_success, links, rocket } = details;
     const imgSrc = links.mission_patch_small;
     const land_success = rocket.first_stage.cores[0].land_success;
 
-    return (<Card style={{ width: '18rem' }}>
+    return (<Card className="Rocket-details-card">
         <div key={flight_number}>
             <div>
-                <img src={imgSrc} alt="mission patch" />
+                <img src={imgSrc} alt="mission patch" className="Rocket-mission-image"/>
             </div>
-            <div>
+            <div className="Rocket-mission-name-flight-number">
                 {mission_name} #{flight_number}
             </div>
-            <div>
+            <div className="Rocket-details-label">
                 Mission Id: {mission_id}
             </div>
-            <div>
+            <div className="Rocket-details-label">
                 Launch Year: {launch_year}
             </div>
-            <div>
+            <div className="Rocket-details-label">
                 Successful Launch: {launch_success ? "true" : "false"}
             </div>
-            <div>
+            <div className="Rocket-details-label">
                 Successful Landing: {land_success ? "true" : "false"}
             </div>
         </div>
